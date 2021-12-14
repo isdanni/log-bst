503b3ad7b40e46d7ae3cfd91feb3345bfbdfb6bf
isdanni <leedanni@gmail.com>
2021-12-14 21:20:02 +0800

    add base-new.cpp

---
+++ src/base-new.cpp
@@ -0,0 +1,7 @@
+#ifndef LOG_BST_LIB
+#    error LOG_BST_LIB not defined.
+#endif
+
+#include <log-bst/common/common.h>
+
+template BUF_SIZE void detail::buffer<char>::append(const char *, const char *);
